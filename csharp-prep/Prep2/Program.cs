9dd37fb4d38700fd6125ada8f7a0782fd09d6bb2
bkpalmer <pal22044@byui.edu>
2023-09-18 22:38:23 -0600

    wrote james bond assignment code

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
-}
+        Console.Write ("What is your grade percentage? ");
+        string userInput = Console.ReadLine ();
+        int gradePercent = int.Parse (userInput);
+        string letter = " ";
+
+       if (gradePercent >= 90)
+       {
+            letter = "A";
+       }
+       else if (gradePercent >= 80)
+       {
+            letter = "B";    
+        }
+       else if (gradePercent >= 70)
+       {
+            letter = "C";
+       }
+       else if (gradePercent >= 60)
+       {
+            letter = "D";
+       }
+       else if (gradePercent < 60)
+       {
+            letter = "F";
+       }
+
+        Console.WriteLine ($"Your grade is {letter}");
+
+       if (gradePercent >= 70)
+       {
+            Console.WriteLine ("Congrats you passed!");
+       }
+       else 
+       {
+            Console.WriteLine ("That's not a passing grade :(, Better luck next time!");
+       }
+
+}}
