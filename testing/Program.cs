5cc4818bae63dbdf69462003dbfe12c3f478df97
bkpalmer <pal22044@byui.edu>
2023-09-20 09:47:24 -0600

    syncing local to online

--- testing/Program.cs
+++ testing/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, other world!");
+
+Console.WriteLine("This is a change to the code");
