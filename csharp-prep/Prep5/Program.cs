7e18d126d3e3e2f42578740542be991eb00f1312
bkpalmer <pal22044@byui.edu>
2023-09-19 22:10:45 -0600

    Wrote the code for the function assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
-    }
-}
+        static void DisplayMessage()
+        {
+            Console.WriteLine ("Welcome to the Program");
+        }
+        static string PromptUserName ()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine ();
+            return name;
+        }
+        static int PromptUserNumber ()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+        static double SquareNumber(int number)
+        {
+            double squaredNumber = Math.Sqrt(number);
+            return squaredNumber;
+        }
+        static void DisplayResult (string userName, double number)
+        {
+            Console.WriteLine ($"{userName}, The square root of your favorite number is {number} ");
+        }
+
+        DisplayMessage();
+        string userName = PromptUserName();
+        int favNumber = PromptUserNumber();
+        double sqrtFavNumber = SquareNumber(favNumber);
+        DisplayResult (userName, sqrtFavNumber);
+
+}}
