55bca0f6c4a2fa5ed6f23390bff140f1185287a3
bkpalmer <pal22044@byui.edu>
2023-09-19 19:46:21 -0600

    Added the code for list assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number = -1;
+
+        do
+        {
+        Console.Write ("'Give me a number (typer 0 when done): ");
+        number = int.Parse(Console.ReadLine());
+
+        if (number != 0)
+        {
+            numbers.Add(number);
+        }
+        } while (number != 0);
+
+        int sum = 0;
+        int largest = 0;
+
+        foreach (int num in numbers)
+        {
+            sum = sum + num;
+            if (num > largest)
+            {
+                largest = num;
+            }
+        }
+
+        int count = numbers.Count ();
+        int average = sum / count;
+
+        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The Largest number is {largest}");
     }
 }
