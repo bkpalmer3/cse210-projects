baa47c518f91d925daaeec9a2d938197250f3a39
bkpalmer <pal22044@byui.edu>
2023-09-19 18:59:01 -0600

    wrote the guess the magic number code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string userInput = "";
+        string playerInput = "";
+        int playerGuess = 0;
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+        do 
+        {
+
+            Console.Write("What is your guess? ");
+            playerInput = Console.ReadLine ();
+            playerGuess = int.Parse (playerInput);
+
+            if (playerGuess > magicNumber)
+            {
+                Console.WriteLine ("Lower");
+            }
+            else if (playerGuess < magicNumber)
+            {
+                Console.WriteLine ("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (playerGuess != magicNumber);
     }
 }
