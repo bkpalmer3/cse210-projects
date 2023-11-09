ee89610884c60d9f1a3a7817e67a19f8fbadd77e
bkpalmer <pal22044@byui.edu>
2023-11-08 17:32:14 -0700

    wrote the inheratence assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment assignment = new MathAssignment("Ben Palmer", "addition","Section 7.3", "Problems 9-12");
+        WritingAssignment assignment2 = new WritingAssignment("Ben Palmer", "history","History has its eyes on you");
+
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine(assignment.GetHomeworkList());
+
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetWritingInformation());
     }
 }
